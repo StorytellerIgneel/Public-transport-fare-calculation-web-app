35fdafa28bbdadec3487c0cfe6669ba1ffefca3f
Rhaja <40422415+DRHAJA@users.noreply.github.com>
2025-09-17 02:59:36 +0800

    Create RouteResult.vue

---
+++ fare_calculation/src/components/RouteResult.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="route">
+    <h3>Route Details</h3>
+    <p><b>Total Fare:</b> RM {{ route.total_fare }}</p>
+    <p><b>Total Hops:</b> {{ route.total_hops }}</p>
+
+    <h4>Path:</h4>
+    <ul>
+      <li v-for="(station, i) in route.path" :key="i">
+        {{ station }}
+      </li>
+    </ul>
+  </div>
+  <div v-else>
+    <p>No route selected yet.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RouteResult",
+  props: {
+    route: {
+      type: Object,
+      required: false,
+    },
+  },
+};
+</script>
+
+
+
